42c1fcef4fedfec0daaa978809845b4a5644cf4a
Zeine <zeineshka2003@gmail.com>
2022-05-13 18:21:40 +0600

    reverse by rune

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,10 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	strR := []rune(input)
+	for i, j := 0, len(strR)-1; i < j; i, j = i+1, j-1 {
+		strR[i], strR[j] = strR[j], strR[i]
+	}
+	output = string(strR)
+	return
 }
